12ad692a624ab7204eb6a988fe2cff397e555da0
moi-sixela <106440915+moi-sixela@users.noreply.github.com>
2022-06-24 13:39:37 +0200

    ok

--- public/mesures/poids.php
+++ public/mesures/poids.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="mesures.css">
     <title>Poids</title>
 </head>
 
 <body>
     <?php require("navbar.php") ?>
+    <form class="form-inline" action="/action_page.php">
+        <input type="date" id="date" placeholder="Date" name="date">
+        <input type="int" id="int" placeholder="Poids (en kg)" name="poids">
+        <button type="submit">Submit</button>
+    </form>
 </body>
 
 </html>
