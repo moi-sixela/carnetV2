ef4755fc7759a8899af91724d62d1eee08d48717
moi-sixela <106440915+moi-sixela@users.noreply.github.com>
2022-07-18 00:25:09 +0200

    ok

--- public/index.php
+++ public/index.php
@@ -1,3 +1,48 @@
+<?php
+
+require("connexion_bdd.php");
+
+if (isset($_POST['usernames'], $_POST['passwords'])) {
+  $usernames = htmlspecialchars($_POST['usernames']);
+  $passwords = htmlspecialchars($_POST['passwords']);
+  $stmt = $bdd->prepare("SELECT * FROM `authentification` WHERE `username` = ?");
+  $stmt->execute(array($usernames));
+  $user = $stmt->fetch();
+
+  if ($user && password_verify($passwords, $user['password'])) {
+    echo "valid!";
+  } else {
+    echo "invalid";
+  }
+}
+
+
+
+
+
+if (isset($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['username'], $_POST['password'])) {
+  $firstname = htmlspecialchars($_POST['first_name']);
+  $lastname = htmlspecialchars($_POST['last_name']);
+  $email = htmlspecialchars($_POST['email']);
+  $username = htmlspecialchars($_POST['username']);
+  $password = htmlspecialchars($_POST['password']);
+  $hash = password_hash($password, PASSWORD_DEFAULT);
+  $req = $bdd->prepare('INSERT INTO `authentification`(`first_name`, `last_name`,`email`,`username`, `password`) VALUES (?,?,?,?,?)');
+  $req->execute(array(
+    $firstname,
+    $lastname,
+    $email,
+    $username,
+    $hash
+  ));
+
+  header("Location: http://localhost/carnetV2/public/");
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,37 +75,37 @@
             </ul>
             <div class="tab-content">
               <div class="tab-pane fade active in" id="signup">
-                <h2 class="text-uppercase text-center"> Sign Up for Free</h2>
-                <form id="signup">
+                <h2 class="text-uppercase text-center"> Sign Up </h2>
+                <form id="signup" action="index.php" method="post">
                   <div class="row">
                     <div class="col-xs-12 col-sm-6">
                       <div class="form-group">
                         <label>First Name<span class="req">*</span> </label>
-                        <input type="text" class="form-control" id="first_name" required data-validation-required-message="Please enter your name." autocomplete="off">
+                        <input type="text" class="form-control" name="first_name" id="first_name" required data-validation-required-message="Please enter your name." autocomplete="off">
                         <p class="help-block text-danger"></p>
                       </div>
                     </div>
                     <div class="col-xs-12 col-sm-6">
                       <div class="form-group">
                         <label> Last Name<span class="req">*</span> </label>
-                        <input type="text" class="form-control" id="last_name" required data-validation-required-message="Please enter your name." autocomplete="off">
+                        <input type="text" class="form-control" name="last_name" id="last_name" required data-validation-required-message="Please enter your name." autocomplete="off">
                         <p class="help-block text-danger"></p>
                       </div>
                     </div>
                   </div>
                   <div class="form-group">
                     <label> Your Email<span class="req">*</span> </label>
-                    <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address." autocomplete="off">
+                    <input type="email" class="form-control" name="email" id="email" required data-validation-required-message="Please enter your email address." autocomplete="off">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
                     <label> Your Username<span class="req">*</span> </label>
-                    <input type="username" class="form-control" id="username" required data-validation-required-message="Please enter your email address." autocomplete="off">
+                    <input type="username" class="form-control" name="username" id="username" required data-validation-required-message="Please enter your email address." autocomplete="off">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
                     <label> Password<span class="req">*</span> </label>
-                    <input type="password" class="form-control" id="password" required data-validation-required-message="Please enter your password" autocomplete="off">
+                    <input type="password" class="form-control" name="password" id="password" required data-validation-required-message="Please enter your password" autocomplete="off">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="mrgn-30-top">
@@ -72,15 +117,15 @@
               </div>
               <div class="tab-pane fade in" id="login">
                 <h2 class="text-uppercase text-center"> Log in</h2>
-                <form id="login">
+                <form id="login" action="" method="post">
                   <div class="form-group">
                     <label> Your Username<span class="req">*</span> </label>
-                    <input type="username" class="form-control" id="username" required data-validation-required-message="Please enter your email address." autocomplete="off">
+                    <input type="username" class="form-control" name="usernames" id="usernames" required data-validation-required-message="Please enter your email address." autocomplete="off">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
                     <label> Password<span class="req">*</span> </label>
-                    <input type="passwords" class="form-control" id="passwords" required data-validation-required-message="Please enter your password" autocomplete="off">
+                    <input type="passwords" class="form-control" name="passwords" id="passwords" required data-validation-required-message="Please enter your password" autocomplete="off">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="mrgn-30-top">
